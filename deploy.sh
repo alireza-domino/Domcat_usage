af2b215e43a49d2cebe5d91a4d6922d05810febd
Alireza Mounesisohi <alireza.mounesisohi@dominodatalab.com>
2022-03-09 13:53:09 -0800

    Pushed from Domino: /u/alireza_mounesisohi/Domcat_tool_usage/runs/6228e0cfa8148417c01b1027

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,5 @@
 set -e
 
 platform_namespace="${platform_namespace:-domino-platform}"
-
-mkdir -p domcat-aws-credentials
-printf "${AWS_ACCESS_KEY_ID}" > domcat-aws-credentials/AWS_ACCESS_KEY_ID
-printf "${AWS_SECRET_ACCESS_KEY}" > domcat-aws-credentials/AWS_SECRET_ACCESS_KEY
-unset AWS_SECRET_ACCESS_KEY
-unset AWS_ACCESS_KEY_ID
-kubectl create secret generic -n ${platform_namespace} domcat-aws-credentials --from-file=domcat-aws-credentials || true
-rm -rf domcat-aws-credentials
-
-if [ "${1}" == "--root" ]
-then
-  JOB="job-root.yaml"
-else
-  JOB="job.yaml"
-fi
-
+JOB="job.yaml"
 kubectl apply -n ${platform_namespace} -f ${JOB}
