38120a29e189d2890e1d6f91e8725b101dde48c6
Alireza Mounesisohi <alireza.mounesisohi@dominodatalab.com>
2022-03-09 14:22:05 -0800

    Pushed from Domino: /u/alireza_mounesisohi/Domcat_tool_usage/runs/6228e0cfa8148417c01b1027

--- clean.sh
+++ clean.sh
@@ -1,3 +1,2 @@
 platform_namespace="${platform_namespace:-domino-platform}"
 kubectl delete -n ${platform_namespace} job domcat
-kubectl delete -n ${platform_namespace} secret domcat-aws-credentials
