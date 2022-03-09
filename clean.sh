724e9c619443bc278edfbb5cc4c4feb0f43e4f99
Alireza Mounesisohi <alireza.mounesisohi@dominodatalab.com>
2022-03-09 14:55:01 -0800

    Pushed from Domino: /u/alireza_mounesisohi/Domcat_tool_usage/runs/6228e0cfa8148417c01b1027

--- clean.sh
+++ clean.sh
@@ -1,2 +1,4 @@
 platform_namespace="${platform_namespace:-domino-platform}"
 kubectl delete -n ${platform_namespace} job domcat
+kubectl delete deployment domcat-output -n $platform_namespace
+kubectl delete pvc domcat-output -n $platform_namespace
